92dc149400b8fa354a6616be18ff01747c6d830b
SamPM092 <sammich4294@gmail.com>
2025-01-20 16:02:34 +0700

    difference using jsx and not

--- tailOne/src/App.jsx
+++ tailOne/src/App.jsx
@@ -1,17 +1,45 @@
+import React from 'react'
 import './App.css'
 
 
-  const App = () => {
+const App = () => {
+  // DENGAN JSX
+  // return (
+  // <div>
+  //   <section className='h-screen bg-red-600 text-xl'>
+  //     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, saepe! Incidunt natus explicabo fuga. Assumenda maxime ea perspiciatis, ducimus nam dolores sapiente blanditiis quas aut nihil placeat fuga, reprehenderit quae.</p>
+  //     <h1 className='bg-randomClor'>Random color</h1>
+  //     <div className="exampleCard">hey</div>
+  //   </section>
+  // </div>
+  // )
+  // }
 
-  return (
-    <div>
-      <section className='h-screen bg-red-600 text-xl'>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, saepe! Incidunt natus explicabo fuga. Assumenda maxime ea perspiciatis, ducimus nam dolores sapiente blanditiis quas aut nihil placeat fuga, reprehenderit quae.</p>
-        <h1 className='bg-randomClor'>Random color</h1>
-        <div className="exampleCard">hey</div>
-      </section>
-    </div>
+
+
+  // TANPA JSX
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "section",
+      {className:"h-screen bg-red-600 text-xl"},
+      React.createElement(
+        "p",
+        null,
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, saepe! Incidunt natus explicabo fuga. Assumenda maxime ea perspiciatis, ducimus nam dolores sapiente blanditiis quas aut nihil placeat fuga, reprehenderit quae."
+      ),
+      React.createElement(
+        "h1",
+        {className:'bg-randomClor'},
+        "Random color."
+      ),
+      React.createElement(
+        "div",
+        {className:'randomClor'},
+        "hey."
+      )
+    )
   )
 }
-
-export default App
+  export default App
