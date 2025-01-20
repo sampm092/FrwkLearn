d906a3dcf2c4736bb77393fe5d2afdee8bfd6432
SamPM092 <sammich4294@gmail.com>
2025-01-20 15:19:48 +0700

    layer in tailwind

--- tailOne/src/App.jsx
+++ tailOne/src/App.jsx
@@ -7,6 +7,8 @@ import './App.css'
     <div>
       <section className='h-screen bg-red-600 text-xl'>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, saepe! Incidunt natus explicabo fuga. Assumenda maxime ea perspiciatis, ducimus nam dolores sapiente blanditiis quas aut nihil placeat fuga, reprehenderit quae.</p>
+        <h1 className='bg-randomClor'>Random color</h1>
+        <div className="exampleCard">hey</div>
       </section>
     </div>
   )
--- tailOne/tailwind.config.js
+++ tailOne/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "randomClor" : "#189470",
+      }
+    },
   },
   plugins: [],
 }
